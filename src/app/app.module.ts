9cbf95b3d68a2a60e53d7bfff5cab39bd4ffaec7
Tair Assimov <tair@assimov.net>
2017-07-27 02:10:24 +0200

    Add phone service and listing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routing';
+
+import { PhoneService } from './services/phone.service';
 
 import { AppComponent } from './app.component';
 import { PhoneListComponent } from './components/phone-list/phone-list.component';
@@ -16,9 +19,12 @@ import { PhoneDetailsComponent } from './components/phone-details/phone-details.
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    PhoneService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/phone-list/phone-list.component.ts
+++ src/app/components/phone-list/phone-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PhoneService } from '../../services/phone.service';
 
 @Component({
   selector: 'app-phone-list',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./phone-list.component.css']
 })
 export class PhoneListComponent implements OnInit {
+  
+  phones;
 
-  constructor() { }
+  constructor(private phone: PhoneService) { }
 
   ngOnInit() {
+    this.phone.getList()
+      .subscribe((phones) => {
+        this.phones = phones;
+      });
   }
 
 }
