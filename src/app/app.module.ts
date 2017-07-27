77f06ad64f4e8d9b0df1d9cedd132035e57d70cc
Tair Assimov <tair@assimov.net>
2017-07-27 20:28:21 +0200

    Add session service with a login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routing';
 
 import { PhoneService } from './services/phone.service';
+import { SessionService } from './services/session.service';
 
 import { AppComponent } from './app.component';
 import { PhoneListComponent } from './components/phone-list/phone-list.component';
 import { PhoneDetailsComponent } from './components/phone-details/phone-details.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PhoneListComponent,
-    PhoneDetailsComponent
+    PhoneDetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    FormsModule,
     HttpModule
   ],
   providers: [
-    PhoneService
+    PhoneService,
+    SessionService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { PhoneDetailsComponent } from './components/phone-details/phone-details.component';
 import { PhoneListComponent } from './components/phone-list/phone-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: PhoneListComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'phone/:id', component: PhoneDetailsComponent },
   { path: '**', redirectTo: '' }
 ];
